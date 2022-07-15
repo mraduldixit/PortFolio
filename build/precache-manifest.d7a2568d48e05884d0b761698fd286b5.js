cc9648ace1d618f1daf730f7ed62458f0db86bee
Mradul Dixit <mraduldixit2002@gmail.com>
2022-07-15 17:39:34 +0530

    second commit

---
+++ build/precache-manifest.d7a2568d48e05884d0b761698fd286b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dc1fc7da0648ed54b4b",
+    "url": "/static/css/main.0852c8c4.chunk.css"
+  },
+  {
+    "revision": "5dc1fc7da0648ed54b4b",
+    "url": "/static/js/main.5dc1fc7d.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8ab1d9c2d1a67905cd6667ffb8fdaea",
+    "url": "/index.html"
+  }
+];
